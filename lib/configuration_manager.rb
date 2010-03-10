eeeb8b6e61f2304bc0413ac7710086e3d09094ab
matteo <matteo@wonsys.net>
2010-03-10 20:11:24 +0100

    [RAILS 3] FIXED RAILS_ROOT and RAILS_ENV: if available use Rails.root and Rails.env instead.

--- lib/configuration_manager.rb
+++ lib/configuration_manager.rb
@@ -1,9 +1,11 @@
 class ConfigurationManager
   # Expects the environment to load.
-  # Second argument is path to a YAML file (defaults to RAILS_ROOT/config/configuration_manager.yml).
+  # Second argument is path to a YAML file (defaults to RAILS_ROOT/config/configuration_manager.yml)
+  # Use Rails.root instead of RAILS_ROOT if Rails 3.
   #
-  def self.new_manager(environment=RAILS_ENV, yaml_file=File.join(RAILS_ROOT,"/config/configuration_manager.yml"))
-    yaml_file = File.expand_path(yaml_file)
+  def self.new_manager(environment, yaml_file)
+    environment ||= begin Rails.env; rescue; RAILS_ENV end
+    yaml_file   ||= File.expand_path(File.join(begin Rails.root; rescue; RAILS_ROOT end, '/config/configuration_manager.yml'))
     if File.exist?(yaml_file)
       ConfigurationManagerHash.new_from_hash(YAML.load(ERB.new(File.read(yaml_file)).result)[environment])
     else
